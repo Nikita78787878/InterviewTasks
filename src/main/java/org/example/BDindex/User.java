f1e76e9c9ed5f98bfecfd8a5654ea09ed41fa28d
Epov Nikita <Nikita.Epov@develonica.ru>
2026-04-20 13:06:14 +0800

    bdIndex: step1

---
+++ src/main/java/org/example/BDindex/User.java
@@ -0,0 +1,28 @@
+package org.example.BDindex;
+
+
+public class User {
+    private final Long id;
+    private final String name;
+    private final int age;
+
+    public User(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
---
+++ src/main/java/org/example/BDindex/UserTable.java
@@ -0,0 +1,112 @@
+package org.example.BDindex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Реализовать in-memory таблицу пользователей с поддержкой индекса.
+ * <p>
+ * Часть 1. Таблица без индекса
+ * <p>
+ * Требования:
+ * - хранить пользователей в порядке вставки
+ * - реализовать метод:
+ * <p>
+ * User findByIdScan(Long id)
+ * <p>
+ * который ищет пользователя полным проходом по таблице
+ * <p>
+ * <p>
+ * Часть 2. Индекс
+ * <p>
+ * Добавить Hash Index:
+ * <p>
+ * Требования:
+ * - при добавлении пользователя обновлять индекс
+ * - реализовать метод:
+ * <p>
+ * User findByIdIndexed(Long id)
+ * <p>
+ * Часть 3.
+ * Составной индекс:
+ * - Реализовать поиск по составному индексу:
+ * <p>
+ * List<User> findByAgeAndName(int age, String name)
+ * <p>
+ * Часть 4. Сравнение (опционально)
+ * <p>
+ * - создать 1_000_000 пользователей
+ * - замерить время:
+ * - findByIdScan
+ * - findByIdIndexed
+ * <p>
+ * <p>
+ * public class User {
+ * <p>
+ * private final Long id;
+ * private final String name;
+ * private final int age;
+ * <p>
+ * public User(Long id, String name, int age) {
+ * this.id = id;
+ * this.name = name;
+ * this.age = age;
+ * }
+ * <p>
+ * public Long getId() {
+ * return id;
+ * }
+ * <p>
+ * public String getName() {
+ * return name;
+ * }
+ * <p>
+ * public int getAge() {
+ * return age;
+ * }
+ * }
+ */
+public class UserTable {
+    List<User> users = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        UserTable table = new UserTable();
+
+        int size = 1_000_000;
+
+        // наполняем
+        for (int i = 0; i < size; i++) {
+            table.insert(new User((long) i, "User" + i, i % 100));
+        }
+
+        long targetId = size - 1;
+
+        // scan
+        long start = System.nanoTime();
+        table.findByIdScan(targetId);
+        long scanTime = System.nanoTime() - start;
+
+        // index
+//            start = System.nanoTime();
+//            table.findByIdIndexed(targetId);
+//            long indexTime = System.nanoTime() - start;
+
+        System.out.println("Scan time:  " + scanTime);
+//            System.out.println("Index time: " + indexTime);
+    }
+
+
+    private User findByIdScan(Long id) {
+        for (User user : users) {
+            if (user.getId().equals(id)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private void insert(User user) {
+        users.add(user);
+    }
+}
